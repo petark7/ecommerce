30988c27d5f8314f28b09118abaf31ccb4698247
petark7 <kuzmanovski.petar@uklo.edu.mk>
2023-07-19 14:58:29 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# vite-template
+ Template to use for new projects (with ESlint and XO), removed boilderplate
---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import './App.css';
+
+const App = () => (
+	<h1>Hello world!</h1>
+);
+
+export default App;
