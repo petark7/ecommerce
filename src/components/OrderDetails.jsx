056c0837bf7255ebeb5e1a777dcd334b65837d09
petark7 <petarkk7@gmail.com>
2023-08-15 13:55:51 +0200

    Update OrderDetails.jsx

--- src/components/OrderDetails.jsx
+++ src/components/OrderDetails.jsx
@@ -1,15 +1,16 @@
+import PropTypes from 'prop-types';
 import { format } from 'date-fns';
 import Button from './Button';
 
 const OrderDetails = ({ id, status, items, total, dateOrdered, buttonAction }) => {
 	const dateFormatted = format(new Date(dateOrdered), 'M/d/yyyy HH:mm');
-
+	const shortID = id.slice(0, 5);
 	return (
 		<div className="flex justify-between items-center pb-5 border-b">
 			<div className="flex flex-col md:flex-row md:justify-around w-full md:items-center">
-				<div className="flex items-center gap-3">
-					<div className="text-lg uppercase font-bold">Order #{id}</div>
-
+				<div className="flex items-center gap-2 flex-row md:flex-row">
+					<div className="text-lg uppercase font-bold">Order</div>
+					<div className="text-lg font-semibold uppercase text-gray-400">#{shortID}</div>
 				</div>
 				<div className="text-lg uppercase">{dateFormatted}</div>
 				<div className="flex gap-5 items-center lg:w-[200px] lg:justify-between">
@@ -33,6 +34,15 @@ const OrderDetails = ({ id, status, items, total, dateOrdered, buttonAction }) =
 			</div>
 		</div>
 	);
+};
+
+OrderDetails.propTypes = {
+	buttonAction: PropTypes.any,
+	dateOrdered: PropTypes.any,
+	id: PropTypes.string,
+	items: PropTypes.array,
+	status: PropTypes.string,
+	total: PropTypes.number
 };
 
 export default OrderDetails;
