3398c0064b31cbf1da4b1c85a5b6af59d0034a5e
petark7 <petarkk7@gmail.com>
2023-09-05 10:55:45 +0200

    Complete order details
    Fix bug with rounding

--- src/components/OrderDetailsSummary.jsx
+++ src/components/OrderDetailsSummary.jsx
@@ -1,27 +1,34 @@
-const OrderDetailsSummary = ({ total }) => (
+import PropTypes from 'prop-types';
+
+const OrderDetailsSummary = ({ total = 0, shipping = 0 }) => (
 	<div className="flex justify-end md:flex-wrap-reverse gap-10">
 		{/* <div className="w-full md:w-fit text-center md:text-left">
-			<div className="font-semibold pb-2">Have questions?</div>
-			<div>Feel free to contact us at support@bluzify.com</div>
-		</div> */}
+				<div className="font-semibold pb-2">Have questions?</div>
+				<div>Feel free to contact us at support@bluzify.com</div>
+			</div> */}
 
 		<div className="w-full md:w-[250px] flex flex-col justify-between gap-3 md:pr-[10px] pr-3">
 			<div className="flex justify-end md:justify-between gap-10">
 				<div>Total</div>
-				<div className="w-[60px] font-semibold">$397.00</div>
+				<div className="w-[60px] font-semibold">${total}</div>
 			</div>
 
 			<div className="flex justify-end md:justify-between gap-10">
 				<div>Shipping</div>
-				<div className="w-[60px] font-semibold">$10.00</div>
+				<div className="w-[60px] font-semibold">${shipping}</div>
 			</div>
 			<div className="w-full h-[1px] bg-gray-100" />
 			<div className="flex justify-end md:justify-between gap-10">
 				<div>Order Total</div>
-				<div className="w-[60px] font-semibold">$405.00</div>
+				<div className="w-[60px] font-semibold">${total + shipping}</div>
 			</div>
 		</div>
 	</div>
 );
+
+OrderDetailsSummary.propTypes = {
+	shipping: PropTypes.number,
+	total: PropTypes.number
+};
 
 export default OrderDetailsSummary;
