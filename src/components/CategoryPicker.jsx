1e67021e0ead513d658ddeadff8b2bb0ac7b1a00
petark7 <petarkk7@gmail.com>
2024-09-25 12:02:15 +0200

    Update CategoryPicker.jsx
    Optimize code

--- src/components/CategoryPicker.jsx
+++ src/components/CategoryPicker.jsx
@@ -3,21 +3,13 @@ import categories from "./CategoryComponent/categories";
 const CategoryPicker = () => {
   return (
     <div className="grid grid-cols-3 gap-5">
-      <CategoryComponent
-        imageSrc={categories[0].imagePath}
-        buttons={categories[0].buttons}
-      />
-      <CategoryComponent
-        imageSrc={categories[1].imagePath}
-        buttons={categories[1].buttons}
-      />
-
-      <CategoryComponent
-        imageSrc={categories[2].imagePath}
-        buttons={categories[2].buttons}
-      />
-      {/* <CategoryComponent />
-      <CategoryComponent /> */}
+      {categories.map((category) => (
+        <CategoryComponent
+          key={category.id}
+          imageSrc={category.imagePath}
+          buttons={category.buttons}
+        />
+      ))}
     </div>
   );
 };
